d5a291617f2029e8265d3c8f6808db24a1b205ee
hp <bindureddy4755@gmail.com>
2021-05-04 11:02:36 +0530

    add css and js demo

--- springbootjpa-thymeleaf/src/main/java/com/example/demo/HelloWorldController.java
+++ springbootjpa-thymeleaf/src/main/java/com/example/demo/HelloWorldController.java
@@ -10,6 +10,7 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("message", "Thymeleaf");
-        return "helloworld";
+        //return "helloworld";
+        return "add-css-js-demo";
     }
 }
